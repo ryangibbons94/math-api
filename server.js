764d622b50d58578a00616e5fe12205bf2fed3ea
ryangibbons94 <96315276+ryangibbons94@users.noreply.github.com>
2022-06-02 21:04:57 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,42 @@
+const { response } = require("express");
+const express = require("express");
+const app = express();
+const PORT = 8000;
+let random = Math.random();
+
+const rappers = {
+  "21 savage": {
+    birthName: random,
+    birthLocation: "London, England",
+    age: 29,
+  },
+  "chance the rapper": {
+    birthName: "Chancelor Johnathan Bennett",
+    birthLocation: "Chicago, Illinois,",
+    age: 29,
+  },
+  unknown: {
+    birthName: "Unknown",
+    birthLocation: "Unknown,",
+    age: 0,
+  },
+};
+
+app.get("/", (request, response) => {
+  response.sendFile(__dirname + "/index.html");
+});
+
+app.get("/api/:name", (request, response) => {
+  const rapperName = request.params.name.toLowerCase();
+  if (rappers[rapperName]) {
+    response.json(rappers[rapperName]);
+  } else {
+    response.json(rappers["unknown"]);
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(
+    `the server is now running on port ${PORT}, you better go catch it`
+  );
+});
