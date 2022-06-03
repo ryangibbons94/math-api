84af26ab8c29cf275b7a7f1180c4f229b9f88ef0
ryangibbons94 <96315276+ryangibbons94@users.noreply.github.com>
2022-06-03 16:51:14 -0400

    added more math questions

--- server.js
+++ server.js
@@ -2,37 +2,139 @@ const { response } = require("express");
 const express = require("express");
 const app = express();
 const PORT = 8000;
-let random = Math.random();
+const cors = require("cors");
 
-const rappers = {
-  "21 savage": {
-    birthName: random,
-    birthLocation: "London, England",
-    age: 29,
-  },
-  "chance the rapper": {
-    birthName: "Chancelor Johnathan Bennett",
-    birthLocation: "Chicago, Illinois,",
-    age: 29,
-  },
-  unknown: {
-    birthName: "Unknown",
-    birthLocation: "Unknown,",
-    age: 0,
-  },
-};
+app.use(cors());
+
+function betweenTwoPerfectSquares() {
+  let num = Math.floor(Math.random() * 225) ** 0.5;
+  let min = Math.floor(num);
+  let max = Math.ceil(num);
+  return {
+    question: `Which two whole numbers is the square root of ${Math.round(
+      num ** 2
+    )} between?`,
+    correctAnswer: `${min} and ${max}`,
+    answers: [
+      `${min} and ${max}`,
+      `${min + 2} and ${max + 2}`,
+      `${min + 1} and ${max + 1}`,
+      `${min - 1} and ${max - 1}`,
+    ],
+  };
+}
+
+function squareRootSimple() {
+  let num1 = Math.floor(Math.random() * 15);
+  let num2 = num1 ** 2;
+  let random = Math.floor(Math.random() * 2);
+  return random > 0
+    ? {
+        question: `What is ${num1} squared?`,
+        correctAnswer: num2,
+        answers: [
+          num2,
+          num2 + Math.floor(Math.random() * 50),
+          num2 + Math.floor(Math.random() * 50),
+          num1 ** 0.5,
+        ],
+      }
+    : {
+        question: `What is the square root of ${num2}?`,
+        correctAnswer: num1,
+        answer: [
+          num1,
+          num1 + Math.floor(Math.random() * 5),
+          num1 - Math.floor(Math.random() * 5),
+          num2 ** 2,
+        ],
+      };
+}
+
+function missingAngle() {
+  let num1 = Math.floor(Math.random() * 180);
+  let num2 = Math.floor(Math.random() * (180 - num1));
+  let num3 = 180 - num1 - num2;
+  return num1 > 9 && num2 > 9 && num3 > 9
+    ? {
+        question: `Two angles in a triangle are ${num1} degrees and ${num2} degrees, what is the measure of the third angle?`,
+        correctAnswer: num3,
+        answers: [num3, num3 + 10, 180, Math.floor(Math.random() * 180)],
+      }
+    : missingAngle();
+}
+
+function proportionalOrNot() {
+  let random = Math.floor(Math.random() * 2);
+  let num1 = Math.ceil(Math.random() * 15);
+  const num2 = Math.ceil(Math.random() * 80);
+  if (random > 0) {
+    return {
+      question: `Does the equation y=${num1}x represent a proportional relationship?`,
+      correctAnswer: "Yes",
+      answers: ["Yes", "No", "Maybe", "None of the above"],
+    };
+  } else {
+    return {
+      question: `Does the equation y=${num1}x+${num2} represent a proportional relationship?`,
+      correctAnswer: "No",
+      answers: ["No", "Yes", "Maybe", "None of the above"],
+    };
+  }
+}
+
+function twoStepEquation() {
+  let num1 = Math.ceil(Math.random() * 15);
+  const num2 = Math.ceil(Math.random() * 80);
+  let num3 = Math.ceil(Math.random() * 15);
+  let random = Math.floor(Math.random() * 2);
+
+  return random > 0
+    ? {
+        question: `What is the value of x? ${num1}x+${num2}=${
+          num1 * num3 - num2
+        }`,
+        correctAnswer: num3,
+        answers: [
+          num3,
+          num3 + Math.ceil(Math.random() * 15),
+          num3 + Math.ceil(Math.random() * 50),
+          num1 * num3 + num2,
+        ],
+      }
+    : {
+        question: `What is the value of x? ${num1}x-${num2}=${
+          num1 * num3 + num2
+        }`,
+        correctAnswer: num3,
+        answers: [
+          num3,
+          num3 + Math.floor(Math.random() * 15),
+          num3 + Math.floor(Math.random() * 15),
+          num1 * num3 - num2,
+        ],
+      };
+}
 
 app.get("/", (request, response) => {
   response.sendFile(__dirname + "/index.html");
 });
 
-app.get("/api/:name", (request, response) => {
-  const rapperName = request.params.name.toLowerCase();
-  if (rappers[rapperName]) {
-    response.json(rappers[rapperName]);
-  } else {
-    response.json(rappers["unknown"]);
-  }
+app.get("/api/", (request, response) => {
+  let betweenTwoSquares = betweenTwoPerfectSquares();
+  let triangle = missingAngle();
+  let equation = twoStepEquation();
+  let squareRoot = squareRootSimple();
+  let proportional = proportionalOrNot();
+  const questions = {
+    triangle: triangle,
+    betweenTwoSquares: betweenTwoSquares,
+    equation: equation,
+    squareRoot: squareRoot,
+    proportional: proportional,
+  };
+
+  response.json(questions);
 });
 
 app.listen(process.env.PORT || PORT, () => {
